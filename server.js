7c518eee4f5f2d9862da2a8ab8e366986e34742b
Nick Husher <nhusher@gmail.com>
2016-12-14 23:40:15 -0500

    Unique rtc session for every client

--- server.js
+++ server.js
@@ -10,6 +10,37 @@ let {
 } = wrtc
 
 let app = express()
+let connections = {}
+
+function doConnect (id, offer) {
+  let pc = new RTCPeerConnection()
+
+  connections[id] = pc
+
+  pc.ondatachannel = evt => {
+    let dc = evt.channel
+
+    dc.onopen = () => {
+      console.log("data channel opened")
+    }
+    dc.onmessage = e => {
+      console.log(e)
+      dc.send('pong')
+    }
+  }
+
+  pc.oniceconnectionstatechange = () => {
+    if(pc.iceConnectionState == 'disconnected') {
+      console.log('Disconnected', id)
+      delete connections[id]
+    }
+  }
+
+  return pc.setRemoteDescription(new RTCSessionDescription(offer))
+    .then(() => pc.createAnswer())
+    .then(answer => pc.setLocalDescription(answer).then(() => answer))
+}
+
 let pc = new RTCPeerConnection()
 pc.ondatachannel = evt => {
   let dc = evt.channel
@@ -26,7 +57,11 @@ pc.ondatachannel = evt => {
 app.use(bodyParser.json())
 app.use(express.static(__dirname))
 
-app.post('/ice-candidate', (req, res) => {
+app.post('/ice-candidate/:sesId', (req, res) => {
+  if (!connections[req.params.sesId]) res.end()
+
+  let pc = connections[req.params.sesId]
+
   pc.addIceCandidate(new RTCIceCandidate(req.body))
     .then(r => {
       res.end()
@@ -36,15 +71,11 @@ app.post('/ice-candidate', (req, res) => {
     })
 })
 
-app.post('/offer', (req, res) => {
-  pc.setRemoteDescription(new RTCSessionDescription(req.body))
-    .then(() => pc.createAnswer())
-    .then(answer => {
-      pc.setLocalDescription(answer).then(() => {
-        res.json(answer)
-        res.end()
-      })
-    })
+app.post('/offer/:sesId', (req, res) => {
+  doConnect(req.params.sesId, req.body).then(answer => {
+    res.json(answer)
+    res.end()
+  })
 })
 
 
